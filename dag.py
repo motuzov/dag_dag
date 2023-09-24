fdbba10b45f423930655be4f37d71ce29eb0001c
motuzov <motuzov_av@nlmk.com>
2023-09-24 21:01:44 +0400

    Config and Resources

--- dag.py
+++ dag.py
@@ -1,71 +1,52 @@
-from dsml4s8e.define_job import define_job
-from dagstermill import local_output_notebook_io_manager
-from dagster import job
-
 from pathlib import Path
 
 import json
 import os
 
-import requests
-from dagster import (asset,
-                     repository,
-                     ScheduleDefinition,
-                     define_asset_job,
-                     AssetSelection)  # import the `dagster` library
+from dagster import (
+    asset,
+    ScheduleDefinition,
+    define_asset_job,
+    AssetSelection,
+    sensor,
+    RunRequest,
+    RunConfig,
+    op,
+    job,
+    Config,
+    EnvVar, Definitions, ConfigurableResource,
+    create_repository_using_definitions_args
+    )  # import the `dagster` library
+
 import pandas as pd
+
+data_dir = '/home/jovyan/work/dagster_home/storage'
 
 
 @asset  # add the asset decorator to tell Dagster this is an asset
-def topstory_ids() -> None:
-    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-    top_new_story_ids = requests.get(newstories_url).json()[:100]
-
-    os.makedirs("data", exist_ok=True)
-    with open("data/topstory_ids.json", "w") as f:
-        json.dump(top_new_story_ids, f)
+def aleks_msg() -> None:
+    hello_msg_str = 'hello liana!'
+    with open(f"{data_dir}/aleks_msg.json", "w") as f:
+        json.dump(hello_msg_str, f)
 
 
-@asset(deps=[topstory_ids])  # this asset is dependent on topstory_ids
-def topstories() -> None:
-    with open("data/topstory_ids.json", "r") as f:
-        topstory_ids = json.load(f)
+@asset(deps=[aleks_msg])
+def to_pd() -> None:
+    with open(f"{data_dir}/aleks_msg.json", "r") as f:
+        aleks_msg_str = json.load(f)
 
-    results = []
-    for item_id in topstory_ids:
-        item = requests.get(
-            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
-        ).json()
-        results.append(item)
-
-        if len(results) % 20 == 0:
-            print(f"Got {len(results)} items so far.")
-
-    df = pd.DataFrame(results)
-    df.to_csv("data/topstories.csv")
-
-
-@job(
-    name='simple_pipeline',
-    tags={"cdlc_stage": "dev"},
-    resource_defs={
-        "output_notebook_io_manager": local_output_notebook_io_manager,
-    }
-)
-def dagstermill_pipeline():
-    module_path = Path(__file__)
-    define_job(
-        root_path=module_path.parent.parent,
-        nbs_sequence=[
-            "data_load/nb_0.ipynb",
-            "data_load/nb_1.ipynb",
-            "data_load/nb_2.ipynb"
-        ]
+    df = pd.DataFrame(
+        {
+            'from': ['aleks'],
+            'to': ['liana'],
+            'msg': ['hello']
+        }
     )
+    df.to_csv(f"{data_dir}/hello_df.csv")
 
 
 assets_job = define_asset_job(
-    "assets_job_all",
+    "assets_job_hello",
     selection=AssetSelection.all()
     )
 
@@ -76,18 +57,48 @@ a_schedule = ScheduleDefinition(
 )
 
 
-@repository(
-    name='my_repo',
-    metadata={
-        'team': 'Team A',
-        'repository_version': '1.2.3',
-        'environment': 'production', 
-        }
- )
-def simple_repository_2():
-    return [dagstermill_pipeline,
-            topstory_ids,
-            topstories,
-            assets_job,
-            a_schedule
-            ]
+class FileConfig(Config):
+    filename: str = 'trigger.txt'
+
+
+@op
+def process_file(context, config: FileConfig):
+    context.log.info(f'log: {config.filename}')
+
+
+@job
+def log_file_job():
+    process_file()
+
+
+MY_DIRECTORY = '/home/jovyan/work/data'
+
+
+@sensor(job=log_file_job)
+def my_directory_sensor():
+    conf = FileConfig()
+    filepath = os.path.join(MY_DIRECTORY, conf.filename)
+    if os.path.isfile(filepath):
+        yield RunRequest(
+            run_config=RunConfig(
+                ops={"process_file": conf}
+            ),
+        )
+
+
+class CredentialsResource(ConfigurableResource):
+    password: str
+
+
+named_repo = create_repository_using_definitions_args(
+    name="a_repo",
+    assets=[aleks_msg, to_pd],
+    schedules=[a_schedule],
+    sensors=[my_directory_sensor],
+    jobs=[assets_job, log_file_job],
+    resources={
+        "credentials": CredentialsResource(
+            password=EnvVar("DB_PASS"),
+            )
+    }
+)
